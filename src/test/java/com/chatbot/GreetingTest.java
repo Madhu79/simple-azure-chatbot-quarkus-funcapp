93a1f110d1b93e33b5df6f315f3a1a8318f95993
Madhu79 <madhu.pisipati@gmail.com>
2025-09-07 16:33:05 +1000

    Update GreetingTest.java
    updated test cases

--- src/test/java/com/chatbot/GreetingTest.java
+++ src/test/java/com/chatbot/GreetingTest.java
@@ -8,13 +8,50 @@ import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
 class GreetingTest {
+
     @Test
-    void testHelloEndpoint() {
+    public void testPostHelloEndpoint() {
         given()
-          .when().get("/hello")
+          .contentType(MediaType.APPLICATION_JSON)
+          .body("{\"message\": \"Hello from a test!\"}")
+        .when()
+          .post("/api/hello")
+        .then()
+          .statusCode(200);
+    }
+
+    // Test a GET request for a simple greeting
+    @Test
+    public void testGetHelloEndpoint() {
+        given()
+          .when()
+            .get("/api/hello")
           .then()
-             .statusCode(200)
-             .body(is("hello"));
+            .statusCode(200);
+    }
+
+    // Test a PUT request to update an entire message
+    @Test
+    public void testPutMessageEndpoint() {
+        given()
+          .contentType(MediaType.APPLICATION_JSON)
+          .body("{\"message\": \"This is an updated message.\", \"id\": \"123\"}")
+        .when()
+          .put("/api/messages/123")
+        .then()
+          .statusCode(200);
+    }
+    
+    // Test a PATCH request to partially update a message
+    @Test
+    public void testPatchMessageEndpoint() {
+        given()
+          .contentType(MediaType.APPLICATION_JSON)
+          .body("{\"status\": \"read\"}")
+        .when()
+          .patch("/api/messages/456")
+        .then()
+          .statusCode(200);
     }
 
 }
